af308c0757342baf66267da1a50a793ee3735f0d
cynthiazhang88 <cynthiez96@gmail.com>
2020-07-19 14:27:23 -0400

    read CSV file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,11 @@
 import os
 import csv
 
-total_votes=0
-khan_votes=0
-correy_votes=0
-li_votes=0
-otooley_votes=0
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 csvpath = os.path.join("PyPoll", "Resources", "election_data.csv")
 
@@ -14,13 +14,25 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(csvfile)
 
     for row in csvreader:
-        if row[2]=="Khan":
-            winner="Khan"
-        elif row[2]=="Correy":
-            winner="Correy"
-        elif row[2]=="Li":
-            winner="Li"
+        total_votes += 1
+
+        if row[2] == "Khan":
+            khan_votes += 1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li":
+            li_votes += 1
         else:
-            winner="O'Tooley"    
+            otooley_votes +=1
+        
+
+
+
+
+
+
+
+
+
     
     
