5b1440300a543a5cf4691174fa56cf9d700f5319
cynthiazhang88 <cynthiez96@gmail.com>
2020-07-19 17:16:49 -0400

    printed analysis and exported text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -51,6 +51,17 @@ print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
 print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
 
+output_file = os.path.join('PyBank', 'Resources', 'budget_data_revised.text')
+with open(output_file, 'w',) as txtfile:
+    txtfile.write(f"Financial Analysis\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Months: {total_months}\n")
+    txtfile.write(f"Total: ${net_amount}\n")
+    txtfile.write(f"Average Change: ${average_change}\n")
+    txtfile.write(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})\n")
+    txtfile.write(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})\n")
+
+
 
 
 
